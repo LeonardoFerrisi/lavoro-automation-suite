39fb14cd86720cc93e493fbba2b4f502f651824f
LeonardoFerrisi <biggestresurgence1492@gmail.com>
2022-12-20 23:37:02 -0500

    updates

--- app.py
+++ app.py
@@ -1,8 +1,83 @@
 import streamlit as st
 import streamlit_authenticator as stauth
 import database as db
-import userapplication as userapp
-from userapplication import change_state, check_state, log_user, unlog_user
+
+# User application
+
+def change_state(state="False"):
+    with open('pages/state.txt','r',encoding='utf-8') as file:
+        data = file.readlines()
+    data[0] = f'{state}\n'
+    with open('pages/state.txt', 'w', encoding='utf-8') as file:
+        file.writelines(data)
+
+def check_state():
+    with open('pages/state.txt','r',encoding='utf-8') as file:
+        data = file.readlines()
+    if data[0] == "True" or data[0] == "True\n":
+        return True
+    else: return False
+
+def check_usr():
+    with open('pages/state.txt','r',encoding='utf-8') as file:
+        data = file.readlines()
+    return data[1]
+
+def log_user(username):
+    with open('pages/state.txt','r',encoding='utf-8') as file:
+        data = file.readlines()
+    usr = data[1]
+    if usr != username and usr != f"{username}\n":
+        data[1] = f'{username}\n'
+        with open('pages/state.txt', 'w', encoding='utf-8') as file:
+            file.writelines(data)
+
+def unlog_user():
+    with open('pages/state.txt','r',encoding='utf-8') as file:
+        data = file.readlines()
+    usr = data[1]
+    if usr != "null" and usr != "null\n":
+        data[1] = "null\n"
+        with open('pages/state.txt', 'w', encoding='utf-8') as file:
+            file.writelines(data)
+
+def create_userapplication(userdata, authenticator:stauth.Authenticate):
+    """
+    userdata is formatted as follows:
+
+    {   username : {
+                        "name"    : user["name"],
+                        "email"   : user["email"],
+                        "password": user["password"],
+                        "data"    : user["data"]
+                    }
+    }
+    """
+
+    change_state(state="True")
+    
+    name = userdata["name"]
+    email = userdata["email"]
+    password = userdata["password"]
+    files = userdata["data"]    
+
+    st.markdown(f'# WELCOME! {name}')
+    
+    if not files:
+        st.info(':red[You dont have any data, would you like to create some?]')
+
+        if st.button('Create data'):
+            st.info("Excellent! Navigate to the sidebar to your left and select **your info**")
+        else:
+            st.write('Goodbye')
+
+    with st.sidebar:
+        authenticator.logout('Logout', 'main')
+
+def create_userdata(authenticator:stauth.Authenticate):
+    with st.sidebar:
+        authenticator.logout('Logout', 'main')
+
 
 class Application:
 
@@ -103,7 +178,7 @@ class Application:
         
         if self.LOGGEDIN:
             userdeta = credentials["usernames"][self.current_user]
-            userapp.create_userapplication(userdeta, authenticator)
+            create_userapplication(userdeta, authenticator)
 
 
 
--- pages/your_info.py
+++ pages/your_info.py
@@ -1,18 +1,18 @@
 import streamlit as st
 import database as db
-from userapplication import check_state, check_usr
+import app as a
 from dotenv import load_dotenv 
 import os
 
 st.header("Your Info")
 
-state = check_state()
+state = a.check_state()
 
-LOGGEDIN = check_state()
+LOGGEDIN = a.check_state()
 
 if LOGGEDIN:
     st.write("Your Information is below")
-    usr = check_usr().replace("\n", "")
+    usr = a.check_usr().replace("\n", "")
     info = db.get_user(usr)
     files = info["data"]
     if files:
